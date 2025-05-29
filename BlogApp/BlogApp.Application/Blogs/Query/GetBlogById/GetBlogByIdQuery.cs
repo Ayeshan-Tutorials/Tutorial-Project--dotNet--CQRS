7ea6d88ff4e60776b79374da3adcfe6b6902882a
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-05-29 18:49:04 +0530

    Query Handler By Id

---
+++ BlogApp/BlogApp.Application/Blogs/Query/GetBlogById/GetBlogByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using BlogApp.Application.Blogs.Query.GetBlogs;
+using MediatR;
+
+namespace BlogApp.Application.Blogs.Query.GetBlogById
+{
+    public class GetBlogByIdQuery : IRequest<BlogVm>
+    {
+        public int BlogId { get; set; }
+    }
+}
---
+++ BlogApp/BlogApp.Application/Blogs/Query/GetBlogById/GetBlogByIdQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BlogApp.Application.Blogs.Query.GetBlogs;
+using BlogApp.Domain.Repository;
+using MediatR;
+
+namespace BlogApp.Application.Blogs.Query.GetBlogById
+{
+    public class GetBlogByIdQueryHandler : IRequestHandler<GetBlogByIdQuery, BlogVm>
+    {
+        private readonly IBlogRepository _blogRepository;
+        private readonly IMapper _mapper;
+        public GetBlogByIdQueryHandler(IBlogRepository blogRepository, IMapper mapper)
+        {
+            _blogRepository = blogRepository;
+            _mapper = mapper;
+        }
+        public async Task<BlogVm> Handle(GetBlogByIdQuery request, CancellationToken cancellationToken)
+        {
+            var blog =  await _blogRepository.GetByIdAsync(request.BlogId);
+
+            return _mapper.Map<BlogVm>(blog);
+        }
+    }
+}
