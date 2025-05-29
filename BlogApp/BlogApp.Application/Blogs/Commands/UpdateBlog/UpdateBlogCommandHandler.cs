6e5c053fc186d43503f206f1b9f0f35efdc29353
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-05-29 19:29:15 +0530

    Command Handler for Update

---
+++ BlogApp/BlogApp.Application/Blogs/Commands/UpdateBlog/UpdateBlogCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using BlogApp.Domain.Entity;
+using BlogApp.Domain.Repository;
+using MediatR;
+
+namespace BlogApp.Application.Blogs.Commands.UpdateBlog
+{
+    public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand, int>
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public UpdateBlogCommandHandler(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<int> Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
+        {
+            var blogEntity = new Blog()
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Author = request.Author,
+                Description = request.Description
+            };
+
+            return await _blogRepository.UpdateAsync(request.Id, blogEntity);
+        }
+    }
+}
