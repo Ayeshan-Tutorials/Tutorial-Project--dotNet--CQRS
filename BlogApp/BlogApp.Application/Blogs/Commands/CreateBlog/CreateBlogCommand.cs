82f28e2484a57473092f5356640372696e4aaefc
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-05-29 19:19:34 +0530

    Command Handler for Create

---
+++ BlogApp/BlogApp.Application/Blogs/Commands/CreateBlog/CreateBlogCommand.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Application.Blogs.Query.GetBlogs;
+using MediatR;
+
+namespace BlogApp.Application.Blogs.Commands.CreateBlog
+{
+    public class CreateBlogCommand : IRequest<BlogVm>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ BlogApp/BlogApp.Application/Blogs/Commands/CreateBlog/CreateBlogCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BlogApp.Application.Blogs.Query.GetBlogs;
+using BlogApp.Domain.Entity;
+using BlogApp.Domain.Repository;
+using MediatR;
+
+namespace BlogApp.Application.Blogs.Commands.CreateBlog
+{
+    public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, BlogVm>
+    {
+        private readonly IBlogRepository _blogRepository;
+        private readonly IMapper _mapper;
+        public CreateBlogCommandHandler(IBlogRepository blogRepository, IMapper mapper )
+        {
+            _blogRepository = blogRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<BlogVm> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+        {
+            var blogEntity = new Blog()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Author = request.Author
+            };
+
+           var Result =  await _blogRepository.CreateAsync(blogEntity);
+           return _mapper.Map<BlogVm>(Result);
+        }
+    }
+}
