4340c88f488dbcb8cdf8eaacac55b40fcea0ac77
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-05-29 18:02:33 +0530

    Query Handler for Lists

---
+++ BlogApp/BlogApp.Application/Blogs/Query/GetBlogs/GetBlogQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BlogApp.Application.Blogs.Query.GetBlogs
+{
+    public class GetBlogQuery : IRequest<List<BlogVm>>
+    {
+
+    }
+
+    // New Way
+    // public record GetBlogQuery : IRequest<List<BlogVm>>;
+
+}
---
+++ BlogApp/BlogApp.Application/Blogs/Query/GetBlogs/GetBlogQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using BlogApp.Domain.Repository;
+using MediatR;
+
+namespace BlogApp.Application.Blogs.Query.GetBlogs
+{
+    public class GetBlogQueryHandler : IRequestHandler<GetBlogQuery, List<BlogVm>>
+    {
+        private readonly IBlogRepository _blogRepository;
+        public GetBlogQueryHandler(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<List<BlogVm>> Handle(GetBlogQuery request, CancellationToken cancellationToken)
+        {
+            var blogs = await _blogRepository.GetAllBlogsAsync();
+
+            var blogList = blogs.Select( x => new BlogVm { Author =  x.Author, Name = x.Name, Description = x.Description }).ToList();
+
+            return blogList;
+        }
+    }
+}
