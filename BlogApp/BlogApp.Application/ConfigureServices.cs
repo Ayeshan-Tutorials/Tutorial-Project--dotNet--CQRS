f0c3fa9778754ec0abd6b739e2387b8ed7d0b527
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-05-31 11:38:04 +0530

    Data Validation with Fluent Api

--- BlogApp/BlogApp.Application/ConfigureServices.cs
+++ BlogApp/BlogApp.Application/ConfigureServices.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using BlogApp.Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
 
 namespace BlogApp.Application
 {
@@ -8,9 +11,13 @@ namespace BlogApp.Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(ctg =>
             {
                 ctg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+                // Validation
+                ctg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             });
 
             return services;
